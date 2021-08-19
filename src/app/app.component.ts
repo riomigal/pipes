c1b8851ca9650bef49200c82c4ef8af6b899df42
Saverio Migale <saveriomigale@MacBook-Pro.local>
2021-08-19 12:20:12 +0200

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  currency: number = 0;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onCurrencyChange(value: string) {
+    this.currency = parseFloat(value);
+  }
 }
